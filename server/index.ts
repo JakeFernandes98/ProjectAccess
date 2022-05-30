6982394bcd4c17844f87a99204446df80d457763
JakeFernandes98 <jakeneilaldo@hotmail.com>
2022-05-30 23:07:45 +0100

    feat: db init

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = 8000;
+const port = 8080;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');

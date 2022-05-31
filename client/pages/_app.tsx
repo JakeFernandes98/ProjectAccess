d1f38f71121f85de37af65e6bab42d0d80bcb30a
Anna Hekkink <annhekki@WKMZTD6526D0.global.publicisgroupe.net>
2022-05-31 13:14:49 +0100

    set up themes and created button component

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import theme from '../theme/theme'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider> 
 }
 
 export default MyApp

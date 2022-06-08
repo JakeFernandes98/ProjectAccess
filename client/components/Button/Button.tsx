8529c552d58df6d08752106c646dcf50d40f3826
mariarosello <maria.victoriarosello@e.lloydsbanking.com>
2022-06-08 15:52:38 +0100

    feat: dummy commit

--- client/components/Button/Button.tsx
+++ client/components/Button/Button.tsx
@@ -1,10 +1,12 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import ButtonProps from './Button.types';
+import * as React from "react";
 
-export default function BasicButtons({label, color}:ButtonProps) {
+import Button from "@mui/material/Button";
+import ButtonProps from "./Button.types";
+
+export default function BasicButtons({ label, color }: ButtonProps) {
   return (
-      <Button color={color} variant="contained">{label}</Button>
+    <Button color={color} variant="contained">
+      {label}
+    </Button>
   );
 }

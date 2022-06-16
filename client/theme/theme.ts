72bd034e542048312a750003b4bc6c996c36c1a9
Anna Hekkink <annhekki@WKMZTD6526D0.global.publicisgroupe.net>
2022-06-16 12:40:58 +0100

    feat: reformatted ind ex.tsx for homepage and minor adjustments to prettier config

--- client/theme/theme.ts
+++ client/theme/theme.ts
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { red } from '@mui/material/colors';
+import { red, blue } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
     primary: {
+      main: blue[500],
+    },
+    secondary: {
       main: red[500],
     },
   },

109e6cd27b0d7d3c48717fa7e1a4627755b4dc4b
Anna Hekkink <annhekki@WKMZTD6526D0.global.publicisgroupe.net>
2022-07-06 17:33:09 +0100

    config: cleaned index.tsx and moved experimentation with components to a new page called sandbox.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,148 +1,16 @@
 import type { NextPage } from 'next';
-
-import Button from '../components/Button/Button';
-import TextField from '../components/TextField/TextField';
 import Typography from '../components/Typography/Typography';
-import Avatar from '../components/Avatar/Avatar';
-import Checkbox from '../components/Checkbox/Checkbox';
-import Box from '../components/Box/Box';
-import Grid from '../components/Grid/Grid';
-import Container from '../components/Container/Container';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div>
-        <Typography
-          variant="h2"
-          component="h1"
-          align="center"
-          gutterBottom
-        >
-          Hello, welcome to our homepage
-        </Typography>
-        <Typography
-          variant="body1"
-          component="p"
-          align="center"
-          color="primary"
-        >
-          This is an example of our body typography text using the primary
-          color.
-        </Typography>
-        <Typography
-          variant="body1"
-          component="p"
-          align="center"
-          color="secondary"
-          gutterBottom
-        >
-          This is an example using the secondary color.
-        </Typography>
-      </div>
-      <Container
-        component="main"
-        maxWidth="xs"
+      <Typography
+        variant="body1"
+        component="h1"
+        align="center"
       >
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Grid
-            container
-            columns={12}
-            direction="column"
-            rowSpacing={1}
-          >
-            <Grid
-              container
-              item
-              xs={6}
-              justifyContent="center"
-            >
-              <Avatar
-                src="https://i0.wp.com/sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png?ssl=1"
-                alt="Avatar image"
-                sx={{ width: 48, height: 48 }}
-              ></Avatar>
-            </Grid>
-            <Grid
-              container
-              item
-              xs={6}
-              justifyContent="center"
-            >
-              <TextField
-                label="Email text field"
-                variant="filled"
-                type="email"
-              ></TextField>
-            </Grid>
-            <Grid
-              container
-              item
-              xs={6}
-              justifyContent="center"
-            >
-              <TextField
-                label="Password text field"
-                variant="filled"
-                type="password"
-              ></TextField>
-            </Grid>
-          </Grid>
-          <Grid
-            container
-            direction="row"
-            rowSpacing={3}
-          >
-            <Grid
-              container
-              item
-              xs={3}
-            />
-            <Grid
-              container
-              item
-              xs={6}
-              justifyContent="flex-start"
-            >
-              <Checkbox label="My Checkbox" />
-            </Grid>
-            <Grid
-              container
-              item
-              xs={3}
-            />
-            <Grid
-              container
-              item
-              xs={3}
-            />
-            <Grid
-              container
-              item
-              xs={6}
-            >
-              <Button
-                label="Sign In"
-                variant="contained"
-                color="primary"
-                fullWidth
-              ></Button>
-            </Grid>
-            <Grid
-              container
-              item
-              xs={3}
-            />
-          </Grid>
-        </Box>
-      </Container>
+        Landing page.
+      </Typography>
     </div>
   );
 };
---
+++ client/pages/sandbox.tsx
@@ -0,0 +1,186 @@
+import type { NextPage } from 'next';
+
+import Button from '../components/Button/Button';
+import TextField from '../components/TextField/TextField';
+import Typography from '../components/Typography/Typography';
+import Avatar from '../components/Avatar/Avatar';
+import Checkbox from '../components/Checkbox/Checkbox';
+import Box from '../components/Box/Box';
+import Grid from '../components/Grid/Grid';
+import Container from '../components/Container/Container';
+import List from '../components/List/List/List';
+import ListItem from '../components/List/ListItem/ListItem';
+import ListItemButton from '../components/List/ListItemButton/ListItemButton';
+import ListItemIcon from '../components/List/ListItemIcon/ListItemIcon';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const Sandbox: NextPage = () => {
+  return (
+    <div>
+      <div>
+        <Typography
+          variant="h2"
+          component="h1"
+          align="center"
+          gutterBottom
+        >
+          Sandbox page
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          align="center"
+          gutterBottom
+        >
+          Just a page to test out the components
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          align="center"
+          color="primary"
+        >
+          This is an example of our body typography text using the primary
+          color.
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          align="center"
+          color="secondary"
+          gutterBottom
+        >
+          This is an example using the secondary color.
+        </Typography>
+        <List>
+          <ListItem divider>
+            <ListItemButton
+              primary="Primary text"
+              secondary="Secondary text"
+            >
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+            </ListItemButton>
+          </ListItem>
+          <ListItem divider>
+            <ListItemButton
+              primary="Primary text2"
+              secondary="Secondary text2"
+            >
+              <ListItemIcon>
+                <DeleteIcon />
+              </ListItemIcon>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </div>
+      <Container
+        component="main"
+        maxWidth="xs"
+      >
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Grid
+            container
+            columns={12}
+            direction="column"
+            rowSpacing={1}
+          >
+            <Grid
+              container
+              item
+              xs={6}
+              justifyContent="center"
+            >
+              <Avatar
+                src="https://i0.wp.com/sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png?ssl=1"
+                alt="Avatar image"
+                sx={{ width: 48, height: 48 }}
+              ></Avatar>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={6}
+              justifyContent="center"
+            >
+              <TextField
+                label="Email text field"
+                variant="filled"
+                type="email"
+              ></TextField>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={6}
+              justifyContent="center"
+            >
+              <TextField
+                label="Password text field"
+                variant="filled"
+                type="password"
+              ></TextField>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            rowSpacing={3}
+          >
+            <Grid
+              container
+              item
+              xs={3}
+            />
+            <Grid
+              container
+              item
+              xs={6}
+              justifyContent="flex-start"
+            >
+              <Checkbox label="My Checkbox" />
+            </Grid>
+            <Grid
+              container
+              item
+              xs={3}
+            />
+            <Grid
+              container
+              item
+              xs={3}
+            />
+            <Grid
+              container
+              item
+              xs={6}
+            >
+              <Button
+                label="Sign In"
+                variant="contained"
+                color="primary"
+                fullWidth
+              ></Button>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={3}
+            />
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default Sandbox;
